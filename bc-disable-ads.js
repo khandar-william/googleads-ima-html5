04ecd78a821ae92a120a52d0616d8cc7e706afe3
khandar-william <khandar.william@gdplabs.id>
2019-07-15 13:31:08 +0800

    onload

--- bc-disable-ads.js
+++ bc-disable-ads.js
@@ -1,6 +1,8 @@
-const players = videojs.getPlayers();
-const myPlayer = players[Object.keys(players)[0]];
+window.addEventListener('load', () => {
+  const players = videojs.getPlayers();
+  const myPlayer = players[Object.keys(players)[0]];
 
-myPlayer.ready(() => {
-  myPlayer.ima3.settings.serverUrl = '';
+  myPlayer.ready(() => {
+    myPlayer.ima3.settings.serverUrl = '';
+  });
 });
