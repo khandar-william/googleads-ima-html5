66a44b22c8c40ba1d1c96405fcdf8e6afb512dcc
khandar-william <khandar.william@gdplabs.id>
2019-07-15 14:01:12 +0800

    meta

--- bc-disable-ads.js
+++ bc-disable-ads.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
   const players = videojs.getPlayers();
   const myPlayer = players[Object.keys(players)[0]];
 
-  myPlayer.one('loadeddata', () => {
+  myPlayer.one('loadedmetadata', () => {
     myPlayer.ima3.settings.serverUrl = '';
     myPlayer.ima3.adsLoader.getSettings().setAutoPlayAdBreaks(false);
     myPlayer.ima3.adsManager.discardAdBreak();
